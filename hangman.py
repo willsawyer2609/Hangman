68cb65508de0ef3251e8930f60bad93a205d37f4
willsawyer2609 <wmsawyer2609@gmail.com>
2021-07-20 22:16:48 +1000

    Initial commit.

---
+++ hangman.py
@@ -0,0 +1,41 @@
+# secret_word = list(input("Enter a word: ").lower())
+secret_word = list("i love you")
+redacted = ["_"]*len(secret_word)
+
+badguess = 0
+letters = []
+status = "in progess"
+
+def hangman(secret_word, redacted, badguess, letters):
+    """ Asks player to guess a letter of the word.
+
+    parameters
+    ----------
+    secret_word (str): word for the player to guess.
+    """
+    empty = ["______ ", "| ", "| ", "| ", "| ", "|_____ "]
+    stages = ["______ ", "|  | ", "|  0 ", "| /|\ ", "| / \ ", "|_____ "]
+    print("Guess the word:", " ".join(redacted) + "\n" + "\n".join(stages[:badguess] + empty[badguess:]) + "\n")
+    letter = input("Enter a letter: ").lower()
+    if letter in letters:
+        print("You already tried '" + letter + "'. Guess a different letter next time!\n\n")
+        return redacted, badguess, letters
+    elif letter in secret_word:
+        for index, char in enumerate(secret_word):
+            if char == letter:
+                redacted[index] = secret_word[index]
+        print("Good guess! '" + letter + "' is in the word.\n\n")
+    else:
+        badguess += 1
+        print("Bad guess! '" + letter + "' is not in the word.\n\n")
+    letters.append(letter)
+    return redacted, badguess, letters
+
+while status != "finished":
+    redacted, badguess, letters = hangman(secret_word, redacted, badguess, letters)
+    if redacted == secret_word:
+        status = "finished"
+        print("You win!\nThe word was " + "".join(redacted) + "!")
+    if badguess == len(stages):
+        status = "finished"
+        print("Game over. The word was '" + "".join(secret_word) + "'.")
