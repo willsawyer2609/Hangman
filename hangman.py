f6238ace315328adbd3484f593f89fd05f75126b
William Sawyer <wmsawyer2609@gmail.com>
2021-09-20 21:54:22 +1000

    Utilised formatted strings.

--- hangman.py
+++ hangman.py
@@ -15,19 +15,19 @@ def hangman(secret_word, redacted, badguess, letters):
     """
     empty = ["______ ", "| ", "| ", "| ", "| ", "|_____ "]
     stages = ["______ ", "|  | ", "|  0 ", "| /|\ ", "| / \ ", "|_____ "]
-    print("Guess the word:", " ".join(redacted) + "\n" + "\n".join(stages[:badguess] + empty[badguess:]) + "\n")
+    print(f"Guess the word:{" ".join(redacted)}\n{"\n".join(stages[:badguess] + empty[badguess:])}\n")
     letter = input("Enter a letter: ").lower()
     if letter in letters:
-        print("You already tried '" + letter + "'. Guess a different letter next time!\n\n")
+        print(f"You already tried '{letter}'. Guess a different letter next time!\n\n")
         return redacted, badguess, letters
     elif letter in secret_word:
         for index, char in enumerate(secret_word):
             if char == letter:
                 redacted[index] = secret_word[index]
-        print("Good guess! '" + letter + "' is in the word.\n\n")
+        print(f"Good guess! '{letter}' is in the word.\n\n")
     else:
         badguess += 1
-        print("Bad guess! '" + letter + "' is not in the word.\n\n")
+        print(f"Bad guess! '{letter}' is not in the word.\n\n")
     letters.append(letter)
     return redacted, badguess, letters
 
@@ -35,7 +35,7 @@ while status != "finished":
     redacted, badguess, letters = hangman(secret_word, redacted, badguess, letters)
     if redacted == secret_word:
         status = "finished"
-        print("You win!\nThe word was " + "".join(redacted) + "!")
+        print(f"You win!\nThe word was {"".join(redacted)}!")
     if badguess == len(stages):
         status = "finished"
-        print("Game over. The word was '" + "".join(secret_word) + "'.")
+        print(f"Game over. The word was '{"".join(secret_word)}'.")
